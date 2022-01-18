1bae119fc45887db7259da27139c7e60ab6144ea
Ryan Nordeen <ryannordeen@gmail.com>
2022-01-18 00:29:40 -0600

    Adding macros

--- keyboards/handwired/sixpack/keymaps/default/keymap.c
+++ keyboards/handwired/sixpack/keymaps/default/keymap.c
@@ -6,7 +6,8 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
-    _FN
+    _FN,
+    _MACROS
 };
 
 /*
@@ -19,15 +20,70 @@ XXXXXXX : No operation (No Op)
 RESET   : Reset into bootloader
 KC_LCMD : Left GUI (Windows/Command/Meta Key)
 
- */
+*/
+
+/*
+Macros
+*/
+#define _OPEN_APP( app_name )\
+    do{\
+    SEND_STRING( SS_TAP( X_LGUI ) SS_DELAY( 500 ) app_name  SS_DELAY( 500 ) SS_TAP( X_ENT ) ); \
+    } while ( 0 )
+
+enum custome_keycodes {
+    STEAM=SAFE_RANGE, // SAFE_RANGE prevents it from using a clashing keycode
+    NAUTILUS,
+    FIREFOX,
+    VLC,
+};
+
+bool process_record_user( uint16_t keycode, keyrecord_t *record ) {
+    switch( keycode ){
+    case STEAM:
+        if ( record->event.pressed ) {
+            _OPEN_APP( "steam" );
+        } else {
+            // when keycode is released
+        }
+        break;
+    case NAUTILUS:
+        if ( record->event.pressed ) {
+            _OPEN_APP( "nautilus" );
+        } else {
+            // when released
+        }
+        break;
+    case FIREFOX:
+        if ( record->event.pressed ) {
+            _OPEN_APP( "firefox" );
+        } else {
+            // when released
+        }
+        break;
+    case VLC:
+        if ( record->event.pressed ) {
+            _OPEN_APP( "vlc" );
+        } else {
+            // when released
+        }
+        break;
+    }
+    return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-        KC_Q,    KC_W, MO(_FN),
-        KC_A,    KC_S, KC_D
+        MO(_MACROS), KC_UP,   MO(_FN),
+        KC_LEFT,     KC_DOWN, KC_RIGHT
     ),
     [_FN] = LAYOUT(
-        KC_PGUP,  KC_PGDN,   _______,
+        KC_TILDE, KC_INSERT, _______,
         RESET,    KC_LCMD,   XXXXXXX
+    ),
+    [_MACROS] = LAYOUT(
+         _______,  STEAM,     NAUTILUS,
+         FIREFOX,  VLC,       XXXXXXX
     )
+
 };
